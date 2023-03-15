8acc13ae5798afff07d867160c7f2bbbb9a84bf8
abdulrab9939 <abdulrab4546@gmail.com>
2023-03-15 14:14:12 +0530

    third change

--- src/components/Register.js
+++ src/components/Register.js
@@ -95,7 +95,7 @@ export default function Register( {
     return (
         <div className='register-main'>
            <ToastContainer/>
-            <h1>Registerhghnsfasdf</h1>
+            <h1>Registerhghnsfasdffas</h1>
             {successMsg&&<>
             <div className='success-msg'>
                 {successMsg}
